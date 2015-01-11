5fd7de421c610ad7b4e857afffcda01df68d3349
Chang Junwei <mingle890805@gmail.com>
2015-01-11 18:20:43 +0800

    Initial Commit

---
+++ MorseKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MorseKeyboard
+//
+//  Created by Mingle Chang on 15/1/11.
+//  Copyright (c) 2015年 Mingle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
